7b604104c610063035f334963ea54df875cb4c0d
dacelisl <hero055@gmail.com>
2019-08-22 23:12:26 -0500

    config file webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCSSExtract = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
@@ -16,12 +17,34 @@ module.exports = {
           use: "css-loader"
         })
       },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+     /*  {
+        test: /\.css$/,
+        use: [
+          MiniCSSExtract.loader,
+          'css-loader'
+        ],
+      } */
    ]
   },
+
   plugins: [
     new ExtractTextPlugin("bundle.css"),
     new HtmlWebpackPlugin({
       template:  path.resolve(__dirname, 'src/index.html'),
-    })
+      filename: './new.html'
+    }),
+  /*   new MiniCSSExtract({
+      filename: '[name].css',
+      chunkFilename:'[id].css'
+    }), */
   ]
 }
